dab0572cf6f44d67d7bf973e809689fe391c4357
Asan Indusara <asanindusara348@gmail.com>
2026-02-21 14:04:22 +0530

    fixed babel config file

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,10 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-      "nativewind/babel",
-    ],
+    presets: ['babel-preset-expo'],
     plugins: [
-      "react-native-reanimated/plugin",
+      'nativewind/babel', // This must be in PLUGINS, not presets
+      'react-native-reanimated/plugin', // This must ALWAYS be last
     ],
   };
 };
-
